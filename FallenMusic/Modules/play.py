2d596f500a488f081c983f5fa08499f6c8957faa
Hyper X Rachit <122621466+Hyper-x-Rachit@users.noreply.github.com>
2023-04-11 12:54:24 +0530

    Update play.py
    Signed-off-by: Hyper X Rachit <122621466+Hyper-x-Rachit@users.noreply.github.com>

--- FallenMusic/Modules/play.py
+++ FallenMusic/Modules/play.py
@@ -67,7 +67,7 @@ from FallenMusic.Helpers.thumbnails import gen_qthumb, gen_thumb
 )
 async def play(_, message: Message):
     fallen = await message.reply_text("» ᴘʀᴏᴄᴇssɪɴɢ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...")
-    await message.reply_text("🔎")
+    await message.edit_text("🔎")
     await message.delete()
     try:
         await message.delete()
